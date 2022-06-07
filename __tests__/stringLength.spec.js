7ac32505e76ff26007659590201e591a38a03443
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-07 22:14:37 +0000

    Add test for string length function

---
+++ __tests__/stringLength.spec.js
@@ -0,0 +1,35 @@
+const stringLength = require('../src/stringLength.js');
+
+describe('Calculate length of string', () => {
+  test('should print the length of string', () => {
+    // Assuming we have name 'john'
+    const input = 'John';
+
+    // the length should be equal to 4
+    const output = 4;
+
+    // Assert
+    expect(stringLength(input)).toEqual(output);
+  });
+
+  test('When passing arguments ensure that the length  is greater that 0', () => {
+    // if user pass a number less that 1 
+    const input = '';
+
+    // it should trow an error
+    expect(() => {
+      stringLength(input);
+    }).toThrowError(Error('string length must be greater that 0'));
+  });
+
+  test('String must be less that or equal to 10', () => {
+
+    // if user pass in a string that is greater that 10 
+    const input = 'I am a very long string';
+
+    // trow and error
+    expect(() => {
+      stringLength(input);
+    }).toThrowError(Error('string length must be less or equal to 10'));
+  });
+});
---
+++ src/stringLength.js
@@ -0,0 +1,6 @@
+const stringLength = (str = '') => {
+  if (str.length > 0 && str.length <= 10) {
+    return str.length;
+  }
+};
+module.exports = stringLength;
